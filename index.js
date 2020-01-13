d1ea2100c7a5068119e92864e0c1dd20d8a89c03
Jeremy Cimafonte <jeremycimafonte@Jeremys-MacBook-Pro.local>
2020-01-12 20:45:11 -0500

    cors

--- index.js
+++ index.js
@@ -25,8 +25,11 @@ express()
   .use('/', routes)
   .get('/', (req, res) => res.render('pages/index'))
   .use(handleErrors)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+  .use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
