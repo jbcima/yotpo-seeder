128ee1ae556ad41fbc8765aa2444138571b6b614
Jarrett Carraway <jarrett.s.carraway@gmail.com>
2020-04-06 15:56:12 -0400

    update getShopKey to remove .co tld

--- routes/questions/controller.js
+++ routes/questions/controller.js
@@ -1,28 +1,30 @@
-const ApplicationError = require.main.require('./classes/ApplicationError')
+const ApplicationError = require.main.require('./classes/ApplicationError');
 
 const getShopKey = (origin, host) => {
-  let shop = origin.toString()
+  let shop = origin.toString();
   if (!origin && host.indexOf('localhost') !== -1) {
-    shop = process.env.TEST_SHOP
+    shop = process.env.TEST_SHOP;
   }
-  return shop = shop.replace('http://', '')
-  .replace('https://', '')
-  .replace('www.', '')
-  .replace('.com', '')
-  .replace('.myshopify', '')
-  .toUpperCase()
-}
+  return (shop = shop
+    .replace('http://', '')
+    .replace('https://', '')
+    .replace('www.', '')
+    .replace('.com', '')
+    .replace('.co', '')
+    .replace('.myshopify', '')
+    .toUpperCase());
+};
 
 const getAddParams = req => {
-  const { 
+  const {
     product_title = false,
     product_url = false,
     question_content = false,
     display_name = false,
     email = false,
     product_image_url = null,
-    product_description = null
-  } = req.body
+    product_description = null,
+  } = req.body;
 
   return {
     product_title,
@@ -31,31 +33,46 @@ const getAddParams = req => {
     display_name,
     email,
     product_image_url,
-    product_description
-  }
-}
+    product_description,
+  };
+};
 
 const validateAddParams = (params, next) => {
-  const { product_title, product_url, review_score, question_content, display_name, email } = params
+  const {
+    product_title,
+    product_url,
+    review_score,
+    question_content,
+    display_name,
+    email,
+  } = params;
   const requiredFields = {
     product_title,
     product_url,
     question_content,
     display_name,
-    email
-  }
+    email,
+  };
 
   Object.keys(requiredFields).forEach(key => {
     if (!requiredFields[key]) {
-      throw new ApplicationError(`Missing required parameter: ${key}`, 400)
+      throw new ApplicationError(`Missing required parameter: ${key}`, 400);
     }
-  })
+  });
 
-  return true
-}
+  return true;
+};
 
 const generateAddPackage = (auth, id, params) => {
-  const { product_title, product_url, question_content, display_name, email, product_image_url, product_description } = params
+  const {
+    product_title,
+    product_url,
+    question_content,
+    display_name,
+    email,
+    product_image_url,
+    product_description,
+  } = params;
   const body = {
     appkey: auth[0],
     sku: id,
@@ -65,43 +82,49 @@ const generateAddPackage = (auth, id, params) => {
     display_name,
     email,
     product_image_url,
-    product_description
-  }
+    product_description,
+  };
 
   return {
     method: 'POST',
     uri: 'https://api.yotpo.com/questions/send_confirmation_mail',
     json: true,
     body,
-  }
-}
+  };
+};
 
-const generateQuestionsPackage = (id, auth) => { 
+const generateQuestionsPackage = (id, auth) => {
   return {
     uri: `https://api.yotpo.com/products/${auth[0]}/${id}/questions`,
-    json: true
-  }
-}
+    json: true,
+  };
+};
 
 const formatReturnPackage = data => {
-  const { status = { code: 200, message: 'OK'} } = data
-  const { questions = [], total_questions = false, total_answers = false, page = false, per_page = false } = data.response || data || {}
-  const bottomline = { total_questions, total_answers }
-  const pagination = { page, per_page, total: total_questions }
+  const { status = { code: 200, message: 'OK' } } = data;
+  const {
+    questions = [],
+    total_questions = false,
+    total_answers = false,
+    page = false,
+    per_page = false,
+  } = data.response || data || {};
+  const bottomline = { total_questions, total_answers };
+  const pagination = { page, per_page, total: total_questions };
   if (status.code == null) {
-    status.code = 200
+    status.code = 200;
   }
   return {
     status,
     questions,
     pagination,
-    bottomline
-  }
-}
+    bottomline,
+  };
+};
 
-module.exports.getShopKey = getShopKey
-module.exports.getAddParams = getAddParams
-module.exports.validateAddParams = validateAddParams
-module.exports.generateAddPackage = generateAddPackage
-module.exports.generateQuestionsPackage = generateQuestionsPackage
-module.exports.formatReturnPackage = formatReturnPackage
+module.exports.getShopKey = getShopKey;
+module.exports.getAddParams = getAddParams;
+module.exports.validateAddParams = validateAddParams;
+module.exports.generateAddPackage = generateAddPackage;
+module.exports.generateQuestionsPackage = generateQuestionsPackage;
+module.exports.formatReturnPackage = formatReturnPackage;
